e55d44a3d60deb9a6c4e47ddf6a13548208dd4f1
FelipeRRoque <felipe.roque@sga.pucminas.br>
2026-05-07 14:28:52 -0300

    fix: adicionando middlewares

--- src/back/routes/userRoutes.js
+++ src/back/routes/userRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const UserController = require('../controllers/userController');
-
+const authMiddleware = require('../middlewares/authMiddleware');
 // CRUD
 router.post('/users', UserController.create);
-router.get('/users', UserController.findAll);
-router.get('/users/:id', UserController.findById);
-router.put('/users/:id', UserController.update);
-router.delete('/users/:id', UserController.delete);
+router.get('/users', authMiddleware, UserController.findAll);
+router.get('/users/:id', authMiddleware, UserController.findById);
+router.put('/users/:id', authMiddleware, UserController.update);
+router.delete('/users/:id', authMiddleware, UserController.delete);
 
 module.exports = router;
